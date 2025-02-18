3b0576f7380b7db7469a170c3a3e0e9e5de3698d
bromfi24 <838700@unizar.es>
2025-02-18 12:23:23 +0100

    Añadido "scroll infinto" en en listado de Pokemon

--- lib/presentation/screens/list/list_screen.dart
+++ lib/presentation/screens/list/list_screen.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:pokeview/domain/entities/pokemon.dart';
 import 'package:pokeview/presentation/providers/pokemons_provider.dart';
 import 'package:pokeview/presentation/screens/list/pokemon_view.dart';
 import 'package:pokeview/presentation/screens/widgets/shared/custom_app_bar.dart';
 import 'package:provider/provider.dart';
-
+import 'package:animate_do/animate_do.dart';
 
 class ListScreen extends StatefulWidget {
   const ListScreen({super.key});
@@ -17,32 +16,93 @@ class ListScreen extends StatefulWidget {
 }
 
 class _ListScreenState extends State<ListScreen> {
+
+  late ScrollController _scrollController;
+  bool isMounted = false;
+
+  @override
+  void initState() {
+    super.initState();
+    // Inicializando el controlador
+    _scrollController = ScrollController();
+    // Agregar un listener para detectar cuando el scroll ha llegado al final
+    _scrollController.addListener(_scrollListener);
+  }
+
+  // El listener que se llama cuando el scroll cambia
+  void _scrollListener() {
+    // Comprobamos si hemos llegado al final de la lista
+    if( (_scrollController.position.pixels + 500) >= _scrollController.position.maxScrollExtent ) {
+      // Si es así, llamamos al método de carga de más Pokémon
+      context.read<PokemonsProvider>().getNextPokemonsList();
+      moveScrollToBottom();
+    }
+  }
+
+    void moveScrollToBottom() {
+    if( _scrollController.position.pixels + 100 <= _scrollController.position.maxScrollExtent ) return;
+      _scrollController.animateTo(
+        _scrollController.position.pixels + 120, 
+        duration: const Duration(milliseconds: 300), 
+        curve: Curves.fastOutSlowIn
+      );
+    }
+
+
+  @override
+  void dispose() {
+    // Asegúrate de liberar el controlador cuando no lo necesites
+    _scrollController.removeListener(_scrollListener);
+    _scrollController.dispose();
+    isMounted = false;
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final discoverProvider = context.watch<PokemonsProvider>();
     return Scaffold(
       appBar: CustomAppBar(title: "Hazte con todos!"),
-      body: PokemonVisualizer(discoverProvider: discoverProvider),
+      body: PokemonVisualizer(discoverProvider: discoverProvider, scrollController: _scrollController ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: ()=> context.pop(),
+        // child: const Icon(Icons.arrow_back_ios_new_outlined),
+        child: discoverProvider.isLoadingMore 
+          ? SpinPerfect(
+              infinite: true,
+              child: const Icon(Icons.refresh_rounded)
+            )
+          : FadeIn(child: const Icon(Icons.arrow_back_ios_new_outlined)),
+      ),
     );
   }
 }
 
-class PokemonVisualizer extends StatelessWidget {
+class PokemonVisualizer extends StatefulWidget {
   const PokemonVisualizer({
     super.key,
     required this.discoverProvider,
+    required this.scrollController,
   });
 
   final PokemonsProvider discoverProvider;
+  final ScrollController scrollController;
 
   @override
-  Widget build(BuildContext context) {
+  State<PokemonVisualizer> createState() => _PokemonVisualizerState();
+}
+
+class _PokemonVisualizerState extends State<PokemonVisualizer> {
+  @override
+  Widget build(BuildContext context) { 
     return ListView.builder(
-    itemCount: discoverProvider.pokemons.length,
-    itemBuilder: (context, index) {
-      final pokemon = discoverProvider.pokemons[index];
-      return PokemonView(pokemon: pokemon);
-    },
+      controller: widget.scrollController, 
+      scrollDirection: Axis.vertical,
+      itemCount: widget.discoverProvider.pokemons.length,
+      itemBuilder: (context, index) {
+        final pokemon = widget.discoverProvider.pokemons[index];
+        return PokemonView(pokemon: pokemon);
+      },
     );
   }
 }
