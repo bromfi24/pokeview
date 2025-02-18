9a4284e43ce98164d3d5cf153ab36de458fe142c
bromfi24 <838700@unizar.es>
2025-02-18 09:50:59 +0100

    Estructura de Main Screen Desarrollada

---
+++ lib/presentation/screens/widgets/shared/custom_text_field.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String labelText;
+  final TextInputType? keyboardType;
+
+  const CustomTextField({
+    super.key,
+    required this.controller,
+    required this.labelText,
+    this.keyboardType,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      keyboardType: keyboardType,
+      decoration: InputDecoration(
+        labelText: labelText,
+        border: const OutlineInputBorder(),
+      ),
+    );
+  }
+}
